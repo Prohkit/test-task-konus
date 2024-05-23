7e0261979ea9a61415a3fe411e58f94807c12b7e
Никита <nikproserg@gmail.com>
2024-05-23 23:16:40 +0800

    fix BookRepository, add ignore case to find books methods

--- src/main/java/com/example/testtaskkonus/repository/BookRepository.java
+++ src/main/java/com/example/testtaskkonus/repository/BookRepository.java
@@ -3,13 +3,14 @@ package com.example.testtaskkonus.repository;
 import com.example.testtaskkonus.domain.Author;
 import com.example.testtaskkonus.domain.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.List;
 
-public interface BookRepository extends JpaRepository<Book, Long> {
-    List<Book> findBooksByTitle(String title);
+public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
+    List<Book> findBooksByTitleIgnoreCase(String title);
 
-    Book findBookByIsbn(String isnb);
+    Book findBookByIsbnIgnoreCase(String isnb);
 
     List<Book> findBooksByAuthors(List<Author> authors);
 }
